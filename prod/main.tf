ceedcd7e4419951d47c4d3249a1733726b9edd79
daniel <aciddaniel@gmail.com>
2021-02-17 19:16:07 +0200

    enabling CORS

--- prod/main.tf
+++ prod/main.tf
@@ -144,6 +144,13 @@ resource aws_api_gateway_deployment apideploy {
    stage_name  = var.env
 }
 
+module "cors" {
+  source          = "squidfunk/api-gateway-enable-cors/aws"
+  version         = "0.3.1"
+  api_id          = aws_api_gateway_rest_api.api.id
+  api_resource_id = aws_api_gateway_resource.resource.id
+}
+
 # ----------------------------------------
 # POST /books
 # ----------------------------------------
