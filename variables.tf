1c23fa001f4201a959981b3f380e750aaba071fd
daniel <aciddaniel@gmail.com>
2021-03-07 15:37:57 +0200

    updating variables

--- variables.tf
+++ variables.tf
@@ -1,17 +1,31 @@
-variable "region" {
+variable region {
   default = "eu-central-1"
 }
 
-variable "profile" {
-  default = "lms2"
+variable profile {
+  default = "lms"
 }
 
-variable "prefix" {
+variable lms_user_profile {
+  default = "lmsuser"
+}
+
+variable prefix {
   default     = "lms"
   description = "organization or service name"
 }
 
-variable "ddb_statelock_table" {
+variable ddb_statelock_table {
   default     = "tf-statelock"
   description = "name of dynamo db table for terraform state locking"
+}
+
+variable ui_bucket_name {
+  default     = "lms-ui-123"
+  description = "name of the S3 bucket to hold UI related static assets"
+}
+
+variable ddb_books_table_name {
+  default     = "Books"
+  description = "name of dynamo db table"
 }
